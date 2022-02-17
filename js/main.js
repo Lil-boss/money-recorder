0ae0dc0a91c3c7c6065ecf90ba33b81f6ba03c81
Lil-boss <rshaon09@gmail.com>
2022-02-17 13:27:59 +0600

    responsive added

--- js/main.js
+++ js/main.js
@@ -1,39 +1,62 @@
+//expense section
 function expenseCal() {
   const incomeInput = document.getElementById("income-input");
   const incomeValue = parseInt(incomeInput.value);
+  if (incomeValue < 0 || isNaN(incomeValue)) {
+    alert("your income is not valid");
+    return false;
+  }
 
-  const foodinput = document.getElementById("food-input");
-  const foodValue = parseInt(foodinput.value);
-  console.log(foodValue);
+  const foodInput = document.getElementById("food-input");
+  const foodValue = parseInt(foodInput.value);
+  if (foodValue < 0 || isNaN(foodValue)) {
+    alert("your Food is not valid");
+    return false;
+  }
   const rentInput = document.getElementById("rent-input");
   const rentValue = parseInt(rentInput.value);
-
-  console.log(rentValue);
-  const clothsInput = document.getElementById("cloth-input");
-  const clothsValue = parseInt(clothsInput.value);
-  console.log(clothsValue);
-
+  if (rentValue < 0 || isNaN(rentValue)) {
+    alert("your Rent is not valid");
+    return false;
+  }
+  const clothInput = document.getElementById("cloth-input");
+  const clothsValue = parseInt(clothInput.value);
+  if (clothsValue < 0 || isNaN(clothsValue)) {
+    alert("your Cloths is not valid");
+    return false;
+  }
   const totalExpenses = foodValue + rentValue + clothsValue;
   const totalBalance = incomeValue - totalExpenses;
-  console.log(totalBalance);
-  if (isNaN(incomeValue, rentValue, foodValue, clothsValue)) {
-    console.log("this not number ");
-  } else {
-    document.getElementById("total-expense").innerText = totalExpenses;
-    document.getElementById("total-balance").innerText = totalBalance;
-  }
-}
 
+  document.getElementById("total-expense").innerText = totalExpenses;
+  document.getElementById("total-balance").innerText = totalBalance;
+}
+// saving section
 function savingCal() {
   const totalBalanceInput = document.getElementById("total-balance");
   const totalValue = parseFloat(totalBalanceInput.innerText);
-  const saveInput = document.getElementById("saveing-input");
+  const saveInput = document.getElementById("saving-input");
   const saveValue = parseFloat(saveInput.value);
 
   const saving = (totalValue * saveValue) / 100;
-  if ((totalValue) => saving) {
+  if (totalValue >= saving) {
     document.getElementById("save-money").innerText = saving;
+    document.getElementById("total-remain").innerText = totalValue - saving;
   } else {
-    console.log("not enough money");
+    alert("your have not enough money to save!!");
   }
+}
+
+//alert bar
+function alert(message) {
+  document.getElementById("alertBar").style.display = "block";
+  const alertText = (document.getElementById("message").innerText = message);
+
+  autoCloseAlert();
+}
+
+function autoCloseAlert() {
+  setTimeout(function () {
+    document.getElementById("alertBar").style.display = "none";
+  }, 3000);
 }

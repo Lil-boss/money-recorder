0f18cc5bcafedfb89b2b7a2c37a15306bfd1555e
Lil-boss <rshaon09@gmail.com>
2022-02-17 13:47:02 +0600

    validation done

--- js/main.js
+++ js/main.js
@@ -3,36 +3,40 @@ function expenseCal() {
   const incomeInput = document.getElementById("income-input");
   const incomeValue = parseInt(incomeInput.value);
   if (incomeValue < 0 || isNaN(incomeValue)) {
-    alert("your income is not valid");
+    alert("your income input is not valid");
     return false;
   }
 
   const foodInput = document.getElementById("food-input");
   const foodValue = parseInt(foodInput.value);
   if (foodValue < 0 || isNaN(foodValue)) {
-    alert("your Food is not valid");
+    alert("your Food input is not valid");
     return false;
   }
   const rentInput = document.getElementById("rent-input");
   const rentValue = parseInt(rentInput.value);
   if (rentValue < 0 || isNaN(rentValue)) {
-    alert("your Rent is not valid");
+    alert("your Rent input is not valid");
     return false;
   }
   const clothInput = document.getElementById("cloth-input");
   const clothsValue = parseInt(clothInput.value);
   if (clothsValue < 0 || isNaN(clothsValue)) {
-    alert("your Cloths is not valid");
+    alert("your Cloths input is not valid");
     return false;
   }
   const totalExpenses = foodValue + rentValue + clothsValue;
   const totalBalance = incomeValue - totalExpenses;
-
+  if (totalExpenses > incomeValue) {
+    alert("your expenses is more than your income");
+    return false;
+  }
   document.getElementById("total-expense").innerText = totalExpenses;
   document.getElementById("total-balance").innerText = totalBalance;
 }
 // saving section
 function savingCal() {
+  debugger;
   const totalBalanceInput = document.getElementById("total-balance");
   const totalValue = parseFloat(totalBalanceInput.innerText);
   const saveInput = document.getElementById("saving-input");
@@ -43,7 +47,12 @@ function savingCal() {
     document.getElementById("save-money").innerText = saving;
     document.getElementById("total-remain").innerText = totalValue - saving;
   } else {
-    alert("your have not enough money to save!!");
+    if (isNaN(saveValue)) {
+      alert("null value is not allowed");
+    } else {
+      alert("your have not enough money to save!!");
+      saveInput.value = "";
+    }
   }
 }
 
